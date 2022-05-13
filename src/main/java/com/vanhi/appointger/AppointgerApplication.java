f938fb6c9024f38062e63f673c7ae94289b77f9b
Mélanie Manivanh <melanie.manivanh@gmail.com>
2022-05-13 23:32:32 +0200

    first commit

---
+++ src/main/java/com/vanhi/appointger/AppointgerApplication.java
@@ -0,0 +1,13 @@
+package com.vanhi.appointger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vanhi/appointger/AppointgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanhi.appointger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
