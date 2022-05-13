5e1f4fac82e8b9d9efea6395cc0e1ae80f2a4f0c
Mélanie Manivanh <melanie.manivanh@gmail.com>
2022-05-13 23:49:19 +0200

    added 1 test

--- src/main/java/com/vanhi/appointger/AppointgerApplication.java
+++ src/main/java/com/vanhi/appointger/AppointgerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppointgerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppointgerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppointgerApplication.class, args);
+    }
 
 }
--- src/test/java/com/vanhi/appointger/AppointgerApplicationTests.java
+++ src/test/java/com/vanhi/appointger/AppointgerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AppointgerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
