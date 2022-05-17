1254700d2f824f3564d2790332af9660ffc425bd
Mélanie Manivanh <melanie.manivanh@gmail.com>
2022-05-17 07:34:47 +0200

    added crossorigin for CORS

--- src/main/java/com/vanhi/appointger/controller/AppointmentController.java
+++ src/main/java/com/vanhi/appointger/controller/AppointmentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Map;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
